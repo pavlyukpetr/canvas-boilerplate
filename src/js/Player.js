6b90bdad73c3e06060193e7371959a162fea95aa
Peter Pavliuk <hartryfock@gmail.com>
2022-02-04 21:12:27 +0200

    add Player

---
+++ src/js/Player.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const gravity = 0.5;
+
+export default class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 100,
+        this.height = 100
+    }
+
+    draw() {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+    }
+}
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,76 @@
-import utils from './utils'
+import utils from './utils';
+import Player from './Player';
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+    x: innerWidth / 2,
+    y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+});
 
 addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
-  init()
-})
+    init();
+});
 
 // Objects
 class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
 
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.closePath()
+    }
 
-  update() {
-    this.draw()
-  }
+    update() {
+        this.draw()
+    }
 }
 
 // Implementation
-let objects
+let objects;
 function init() {
-  objects = []
+    objects = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+    for (let i = 0; i < 400; i++) {
+        // objects.push()
+    }
 }
+
+const player = new Player();
 
 // Animation Loop
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+    player.update();
+    // objects.forEach(object => {
+    //  object.update()
+    // })
 }
 
-init()
-animate()
+init();
+animate();
